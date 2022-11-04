322f89db11a73ad6e0602cfb497a7a409beac076
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-11-04 19:49:04 +0800

    Add files via upload

--- spec/spec_helper_rgt.rb
+++ spec/spec_helper_rgt.rb
@@ -2,18 +2,16 @@
 
 require 'simplecov'
 SimpleCov.start
-
 require 'yaml'
-
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'vcr'
 require 'webmock'
+require_relative '../lib/rgt_api'
 
-require_relative '../lib/rgt.rb'
-CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-RGT_TOKEN=CONFIG['api_key']
-CORRECT = YAML.safe_load(File.read('spec/fixtures/rgt_results.yml'))
+CONFIG = YAML.safe_load(File.read('../config/secrets.yml'))
+RGT_TOKEN = CONFIG['api_key']
+CORRECT = YAML.safe_load(File.read('fixtures/rgt_results.yml'))
 
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'rgt_api'
