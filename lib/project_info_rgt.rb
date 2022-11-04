362b27d2f575494926045d2cd02837f223c21814
Jerryeeeh <107199830+Jerryeeeh@users.noreply.github.com>
2022-11-04 19:44:22 +0800

    Add files via upload

--- lib/project_info_rgt.rb
+++ lib/project_info_rgt.rb
@@ -1,12 +1,11 @@
-
+# frozen_string_literal: true
 
 require 'http'
 require 'yaml'
 
-
 def rgt_api_path(name)
   config = YAML.safe_load(File.read('config/secrets.yml'))
-  'https://serpapi.com/search.json?&engine=google_trends&q='+name+'&dataset=TIMESERIES&api_key='+config['api_key']
+  "https://serpapi.com/search.json?&engine=google_trends&q=#{name}&dataset=TIMESERIES&api_key=#{config['api_key']}"
 end
 
 def call_rgt_url(url)
@@ -14,7 +13,7 @@ def call_rgt_url(url)
 end
 
 def rgt_data(data)
-  data.each { |stock| puts stock}
+  data.each { |stock| puts stock }
 end
 rgt_response = {}
 rgt_results = {}
@@ -28,5 +27,4 @@ rgt_results['search_parameters'] = project['search_parameters']
 
 rgt_results['interest_over_time'] = project['interest_over_time']
 
-
-File.write('/Users/yehanzhang/Desktop/RGT/spec/fixture/rgt_results.yml', rgt_results.to_yaml)
+File.write('/Users/yehanzhang/Desktop/RGT/spec/fixtures/rgt_results.yml', rgt_results.to_yaml)
