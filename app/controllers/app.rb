e46e8dc0eaf76fd97e0ade490a93a733b98d2a30
Yu-Hsuan <44157186+ZweiChen0328@users.noreply.github.com>
2022-11-26 18:49:02 +0800

    Update app.rb

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -22,15 +22,6 @@ module GoogleTrend
       response['Content-Type'] = 'text/html; charset=utf-8'
 
       routing.root do
-        # session[:watching] ||= []
-        # data_record = Repository::For.klass(Entity::RgtEntity).find_stock_names(session[:watching])
-        # session[:watching] = data_record.map(&:stockname)
-        # if data_record.none?
-        #   flash.now[:notice] = 'Search something to get started'
-        # end
-
-        # stocks_list = Views::StocksList.new(projects)
-
         view 'home'
       end
 
@@ -46,8 +37,6 @@ module GoogleTrend
           # GET /project/owner/project
           routing.delete do
             stockname = qry.to_s
-            # session[:watching].delete(stockname)
-
             routing.redirect '/'
           end
 
@@ -68,8 +57,6 @@ module GoogleTrend
                 flash[:error] = 'Having trouble accessing the database'
               end
             end
-            # puts(data_record.query)
-            # session[:watching].insert(0, data_record.query).uniq!
 
             data_record = Repository::For.klass(Entity::RgtEntity).find_stock_name(rgt_url)
             stock = Mapper::DataPreprocessing.new(data_record).to_entity
@@ -77,11 +64,6 @@ module GoogleTrend
             stock_trend = Views::MainPageInfo.new(data_record, stock)
 
             view 'Gtrend', locals: { stock_trend: }
-
-            # rgt_name = stock.query
-            # rgt_dic = stock.risk
-
-            # view 'Gtrend', locals: { name: rgt_name, interest_over_time: rgt_dic }
           end
         end
       end
